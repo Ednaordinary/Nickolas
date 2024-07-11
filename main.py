2260669d47c7dc8758ef4ab6c739ccbf0e8f94bc
BuildTools <imnotednamode@gmail.com>
2024-07-11 00:42:10 -0600

    euehuehueh (jake gyllenhaal on onemillioncheckboxes.com)

--- main.py
+++ main.py
@@ -27,30 +27,43 @@ def scene_runner():
                     asyncio.run_coroutine_threadsafe(
                         coro=current_scene.interaction.edit_original_message("Video only has " + str(frames) + " frames! Please use a video with at least 100 frames."), loop=client.loop)
                 else:
-                    try:
-                        os.mkdir(current_scene.path + "frames")
-                        fps_in = capture.get(cv2.CAP_PROP_FPS)
-                        fps_out = int(80 / frames * fps_in)
-                        width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-                        height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-                        if height > width:
-                            scaleargs = "-vf scale=-2:1280:flags=lanczos,setsar=1:1"
-                        else:
-                            scaleargs = "-vf scale=1280:-2:flags=lanczos,setsar=1:1"
-                        subprocess.check_call("ffmpeg -i " + current_scene.path + "video.mp4 " + scaleargs + " -r " + str(fps_out) + " " + current_scene.path + "frames/%d.png", shell=True)
-                    except Exception as e:
-                        print(repr(e))
-                        asyncio.run_coroutine_threadsafe(
-                            coro=current_scene.interaction.edit_original_message("Something went wrong while processing the video"), loop=client.loop)
+                    os.mkdir(current_scene.path + "frames")
+                    fps_in = capture.get(cv2.CAP_PROP_FPS)
+                    fps_out = int(80 / frames * fps_in)
+                    width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+                    height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+                    if height > width:
+                        scaleargs = "-vf scale=-2:1280:flags=lanczos,setsar=1:1"
                     else:
-                        os.makedirs(current_scene.path + "distorted/sparce", exist_ok=True)
-                        call_code = subprocess.check_call("colmap feature_extractor --database_path " + current_scene.path + "distorted/database.db --image_path " + current_scene.path + "frames --ImageReader.single_camera 1 --ImageReader.camera_model OPENCV --SiftExtraction.use_gpu 1", shell=True)
-                        if call_code != 0:
-
-                        else:
-                            asyncio.run_coroutine_threadsafe(
-                                coro=current_scene.interaction.edit_original_message(
-                                    "Something went wrong while extracting features."), loop=client.loop)
+                        scaleargs = "-vf scale=1280:-2:flags=lanczos,setsar=1:1"
+                    call_code = subprocess.check_call("ffmpeg -i " + current_scene.path + "video.mp4 " + scaleargs + " -r " + str(fps_out) + " " + current_scene.path + "frames/%d.png", shell=True)
+                    if call_code != 0:
+                        asyncio.run_coroutine_threadsafe(
+                            coro=current_scene.interaction.edit_original_message(
+                                "Something went wrong while processing the video"), loop=client.loop)
+                        continue
+                    os.makedirs(current_scene.path + "distorted/sparce", exist_ok=True)
+                    call_code = subprocess.check_call("colmap feature_extractor --database_path " + current_scene.path + "distorted/database.db --image_path " + current_scene.path + "frames --ImageReader.single_camera 1 --ImageReader.camera_model OPENCV --SiftExtraction.use_gpu True", shell=True)
+                    if call_code != 0:
+                        asyncio.run_coroutine_threadsafe(
+                            coro=current_scene.interaction.edit_original_message(
+                                "Something went wrong while extracting features."), loop=client.loop)
+                        continue
+                    call_code = subprocess.check_call("colmap exhaustive_matcher --database_path " + current_scene.path + "distorted/database.db --SiftMatching.use_gpu True", shell=True)
+                    if call_code != 0:
+                        asyncio.run_coroutine_threadsafe(
+                            coro=current_scene.interaction.edit_original_message(
+                                "Something went wrong while matching features. Try a video with more frame overlap!"), loop=client.loop)
+                        continue
+                    call_code = subprocess.check_call("colmap mapper --database_path " + current_scene.path + "distorted/database.db --SiftMatching.use_gpu True --image_path " + current_scene.path + "frames --output_path" + current_scene.path + "distorted/sparse --Mapper.ba_global_function_tolerance=0.000001")
+                    if call_code != 0:
+                        asyncio.run_coroutine_threadsafe(
+                            coro=current_scene.interaction.edit_original_message(
+                                "Something went wrong while mapping frames. Try a video with more frame overlap!"), loop=client.loop)
+                        continue
+                    distorted = os.listdir(current_scene.path + "sparse")
+                    os.makedirs(current_scene.path + "sparse/0", exist_ok=True)
+                    for image in distorted:
 
         time.sleep(0.01)
 
