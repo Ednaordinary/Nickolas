50016b1fdac57d05fef75cd0578b31eece24e282
BuildTools <imnotednamode@gmail.com>
2024-07-24 23:14:32 -0600

    started testing phase

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import torch
 import time
 import cv2
 import os
+import vram
 
 from arguments import ParamGroup
 from gaussian_model import GaussianModel
@@ -29,20 +30,22 @@ scene_queue = []
 # Almost all of this comes from https://github.com/graphdeco-inria/gaussian-splatting/
 # please go check it out!
 
-async def scene_runner():
+async def async_scene_runner():
+    global scene_queue
     while True:
         if scene_queue != []:
             current_scene = scene_queue[0]
+            print(current_scene.path, current_scene.interaction, current_scene.message)
             try:
                 capture = cv2.VideoCapture(current_scene.path + "video.mp4")
             except:
                 asyncio.run_coroutine_threadsafe(
-                    coro=current_scene.interaction.edit_original_message("Failed to load video."), loop=client.loop)
+                    coro=current_scene.message.edit("Failed to load video."), loop=client.loop)
             else:
                 frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
                 if frames < 100:
                     asyncio.run_coroutine_threadsafe(
-                        coro=current_scene.interaction.edit_original_message(
+                        coro=current_scene.message.edit(
                             "Video only has " + str(frames) + " frames! Please use a video with at least 100 frames."),
                         loop=client.loop)
                 else:
@@ -58,46 +61,52 @@ async def scene_runner():
                     call_code = subprocess.check_call(
                         "ffmpeg -i " + current_scene.path + "video.mp4 " + scaleargs + " -r " + str(
                             fps_out) + " " + current_scene.path + "frames/%d.png", shell=True)
+                    vram.allocate("Nickolas")
+                    async for i in vram.wait_for_allocation("Nickolas"):
+                        asyncio.run_coroutine_threadsafe(
+                            coro=current_scene.message.edit(
+                                "Waiting for " + str(i) + " before loading model"), loop=client.loop)
+                        pass
                     if call_code != 0:
                         asyncio.run_coroutine_threadsafe(
-                            coro=current_scene.interaction.edit_original_message(
+                            coro=current_scene.message.edit(
                                 "Something went wrong while processing the video"), loop=client.loop)
                         continue
-                    os.makedirs(current_scene.path + "distorted/sparce", exist_ok=True)
+                    os.makedirs(current_scene.path + "distorted/sparse", exist_ok=True)
                     call_code = subprocess.check_call(
                         "colmap feature_extractor --database_path " + current_scene.path + "distorted/database.db --image_path " + current_scene.path + "frames --ImageReader.single_camera 1 --ImageReader.camera_model OPENCV --SiftExtraction.use_gpu True",
                         shell=True)
                     if call_code != 0:
                         asyncio.run_coroutine_threadsafe(
-                            coro=current_scene.interaction.edit_original_message(
+                            coro=current_scene.message.edit(
                                 "Something went wrong while extracting features."), loop=client.loop)
                         continue
                     asyncio.run_coroutine_threadsafe(
-                        coro=current_scene.interaction.edit_original_message(
+                        coro=current_scene.message.edit(
                             "Finished extracting features"), loop=client.loop)
                     call_code = subprocess.check_call(
                         "colmap exhaustive_matcher --database_path " + current_scene.path + "distorted/database.db --SiftMatching.use_gpu True",
                         shell=True)
                     if call_code != 0:
                         asyncio.run_coroutine_threadsafe(
-                            coro=current_scene.interaction.edit_original_message(
+                            coro=current_scene.message.edit(
                                 "Something went wrong while matching features. Try a video with more frame overlap!"),
                             loop=client.loop)
                         continue
                     asyncio.run_coroutine_threadsafe(
-                        coro=current_scene.interaction.edit_original_message(
+                        coro=current_scene.message.edit(
                             "Finished matching features"),
                         loop=client.loop)
                     call_code = subprocess.check_call(
                         "colmap mapper --database_path " + current_scene.path + "distorted/database.db --SiftMatching.use_gpu True --image_path " + current_scene.path + "frames --output_path" + current_scene.path + "distorted/sparse --Mapper.ba_global_function_tolerance=0.000001")
                     if call_code != 0:
                         asyncio.run_coroutine_threadsafe(
-                            coro=current_scene.interaction.edit_original_message(
+                            coro=current_scene.message.edit(
                                 "Something went wrong while mapping frames. Try a video with more frame overlap!"),
                             loop=client.loop)
                         continue
                     asyncio.run_coroutine_threadsafe(
-                        coro=current_scene.interaction.edit_original_message(
+                        coro=current_scene.message.edit(
                             "Finished mapping frames"),
                         loop=client.loop)
                     call_code = subprocess.check_call(
@@ -158,14 +167,27 @@ async def scene_runner():
                             if iteration == 30000:
                                 progress_bar.close()
                                 scene.save("model")
+                    vram.deallocate("Nickolas")
+                    asyncio.run_coroutine_threadsafe(
+                        coro=current_scene.message.edit(
+                            "Done! (placeholder)"),
+                        loop=client.loop)
         time.sleep(0.01)
+
+def scene_runner():
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(async_scene_runner())
 
 
 class SceneRequest:
-    def __init__(self, path: str, interaction: discord.Interaction):
+    def __init__(self, path: str, interaction: discord.Interaction, message):
         self.path = path
         self.interaction = interaction
+        self.message = message
 
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
 
 @client.slash_command(description="Make a 3D scene from a video")
 async def scene(
@@ -176,10 +198,14 @@ async def scene(
         await interaction.response.send_message(
             "Please upload an mp4. Your current attachment is " + str(video.content_type))
     else:
-        await interaction.response.send_message("Adding to the queue...")
-        await video.save("videos/" + str(interaction.message.id) + "/video.mp4")
+        message = await interaction.response.send_message("Adding to the queue...")
+        if isinstance(message, discord.PartialInteractionMessage):
+            message = await message.fetch()
+            os.makedirs("videos/" + str(message.id))
+        await video.save("videos/" + str(message.id) + "/video.mp4")
         global scene_queue
-        scene_queue.append(SceneRequest(path="videos/" + str(interaction.message.id) + "/", interaction=interaction))
+        scene_queue.append(SceneRequest(path="videos/" + str(message.id) + "/", interaction=interaction, message=message))
+
 
 
 client.run(TOKEN)
